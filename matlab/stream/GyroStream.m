4218720d0682257a2ec01f8ba9e375e07507ee8c
HU Xiao <hux062303@gmail.com>
2019-09-27 17:38:20 +0200

    fix

--- matlab/stream/GyroStream.m
+++ matlab/stream/GyroStream.m
@@ -12,19 +12,41 @@ classdef GyroStream < handle
         end
         function from_csv(obj,filename)
             obj.data = csvread(filename);
-            if size(obj.data) ~= 3
+            if size(obj.data,1) ~= 3
                 obj.data = obj.data';
             end
         end
         function num = num_samples(obj)
             num = size(obj.data,2);
         end
-        function obj=prefilter(obj)
-            obj.data = post_process_L3G4200D_data(obj.data);
+        function obj=prefilter(obj,do_plot)
+            if do_plot == true
+            h = getfigure('gyro sample',[400,400],'center');
+            subplot(h.ax);
+            subplot(3,1,1);title('gyro-x');hold on;grid on;
+            plot(obj.data(1,:));
+            subplot(3,1,2);title('gyro-y');hold on;grid on;
+            plot(obj.data(2,:));
+            subplot(3,1,3);title('gyro-z');hold on;grid on;
+            plot(obj.data(3,:));
+            axis tight;
+            end
+            obj.data = post_process_L3G4200D_data(obj.data,0.5,0.9);
+            if do_plot == true
+            subplot(3,1,1);title('gyro-x');legend('raw','filter');
+            plot(obj.data(1,:));
+            subplot(3,1,2);title('gyro-y');legend('raw','filter');
+            plot(obj.data(2,:));
+            subplot(3,1,3);title('gyro-z');legend('raw','filter');
+            plot(obj.data(3,:));
+            drawnow;
+            waitforbuttonpress;
+            close(h.fig);
+            end
         end
         function q = integrate(obj,dt)
-            if dt ~= obj.last_dt
-                obj.last_q = integrate_gyro_quaternion_uniform(obj.data, dt);
+            if isempty(obj.last_dt) || dt ~= obj.last_dt
+                obj.last_q = integrate_gyro_quaternion_uniform(obj.data, dt, []);
                 obj.last_dt = dt;
             end
             q = obj.last_q;
