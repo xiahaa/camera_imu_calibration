c907e8be20d085f7e4688904a356a4d533ee668b
HU Xiao <hux062303@gmail.com>
2019-09-27 17:34:47 +0200

    fix arctan bug

--- matlab/utils/rotation_matrix_to_axis_angle.m
+++ matlab/utils/rotation_matrix_to_axis_angle.m
@@ -5,5 +5,5 @@ function [axis, angle] = rotation_matrix_to_axis_angle(R)
     vhat = [R(3,2)-R(2,3),R(1,3)-R(3,1),R(2,1)-R(1,2)]';
     sintheta = 0.5 * dot(axis,vhat);
     costheta = 0.5 * (trace(R)-1);
-    angle = arctan2(sintheta,costheta);
+    angle = atan2(sintheta,costheta);
 end
