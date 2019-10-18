f13ce1b1536812d4e9ababb2f8c1e18456cdb96d
DTU Space drone <xiahaa@space.dtu.dk>
2019-10-18 12:41:44 +0200

    fix opencv_model bug

--- matlab/vision/OpenCVCameraModel.m
+++ matlab/vision/OpenCVCameraModel.m
@@ -14,16 +14,19 @@ classdef OpenCVCameraModel < Camera
         end
         
         function imagePoints = project(obj,points)
-            imagePoints = cv.projectPoints(points, [0,0,0], [0,0,0], obj.camera_matrix, obj.dist_coefs);
+            imagePoints = cv.projectPoints(points', [0,0,0], [0,0,0], obj.camera_matrix, 'DistCoeffs', obj.dist_coefs);
+            imagePoints = imagePoints';
         end
         
         function points1 = undistort(obj,points)
-            points1 = cv.undistortPoints(points,obj.camera_matrix, obj.dist_coefs);
+            points1 = cv.undistortPoints(points',obj.camera_matrix, obj.dist_coefs);
+            points1 = points1';
         end
 
         function objpoints = unproject(obj,image_points)
-            undist_image_points = cv.undistortPoints(image_points, ...
+            undist_image_points = cv.undistortPoints(image_points', ...
                 obj.camera_matrix, obj.dist_coefs, 'P', obj.camera_matrix);
+            undist_image_points = undist_image_points';
             undist_image_points = [undist_image_points;ones(1,size(undist_image_points,2))];
             objpoints = obj.inv_camera_matrix * undist_image_points;
         end
